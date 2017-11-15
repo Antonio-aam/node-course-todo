fa6b19f897b57ba7c7e8c5def1a0534bfeb5fee9
Antonio Angosto Magdaleno <aam27.antonio@gmail.com>
2017-11-15 19:49:22 +0100

    Added delete script

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -6,7 +6,7 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
 	}
 	console.log("Connected to MongoDB Server");
 
-	db.collection("Todos").find().toArray().then((docs) => {
+	/*db.collection("Todos").find().toArray().then((docs) => {
 		console.log("Todos");
 		console.log(JSON.stringify(docs, undefined, 2));
 	}, err => {
@@ -17,7 +17,7 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
 		console.log(`Todos count: ${count}`);
 	}, err => {
 		console.log("Unable to fetch todos", err)
-	});
+	});*/
 
 	db.collection("Todos by name").find({name: "Andrew"}).toArray().then((docs) => {
 		console.log("Todos");
@@ -26,5 +26,5 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
 		console.log("Unable to fetch todos", err)
 	});
 
-	db.close();
+	//db.close();
 });
